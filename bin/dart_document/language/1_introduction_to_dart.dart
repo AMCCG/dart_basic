a86be54b99b2d15bfe710992913dd1b8240d88e2
apisit.a <amornchanchaigul@gmail.com>
2024-04-24 21:08:31 +0700

    dart tutorials

---
+++ bin/dart_document/language/1_introduction_to_dart.dart
@@ -0,0 +1,202 @@
+// Hello World
+// #
+import 'dart:io';
+
+void main() {
+  print('Hello, World!');
+  controlFlowStatements();
+  shorthandArrow();
+  var voyager = Spacecraft('Voyager I', DateTime(1977, 9, 5));
+  voyager.describe();
+
+  var voyager3 = Spacecraft.unlaunched('Voyager III');
+  voyager3.describe();
+
+  final yourPlanet = Planet.earth;
+  if (!yourPlanet.isGiant) {
+    print('Your planet is not a "giant planet".');
+  }
+
+  printWithDelay("Async Hello!!!");
+}
+
+// Variables
+// #
+var name = 'Voyager I';
+var year = 1977;
+var antennaDiameter = 3.7;
+var flyByObjects = ['Jupiter', 'Saturn', 'Uranus', 'Neptune'];
+
+// Control flow statements
+// #
+void controlFlowStatements() {
+  print("Control flow statements #");
+  if (year >= 2001) {
+    print("21st century");
+  } else if (year >= 1901) {
+    print("19st century");
+  }
+  for (final object in flyByObjects) {
+    print(object);
+  }
+  for (int month = 1; month <= 12; month++) {
+    print(month);
+  }
+  while (year < 2016) {
+    year += 1;
+  }
+}
+
+// Functions
+int fibonacci(int n) {
+  if (n == 0 || n == 1) return n;
+  return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+var result = fibonacci(20);
+
+void shorthandArrow() {
+  flyByObjects.where((name) => name.contains('turn')).forEach(print);
+}
+
+class Spacecraft {
+  String name;
+  DateTime? launchDate;
+
+  // Read-only non-final property
+  int? get launchYear => launchDate?.year;
+
+  // Constructor, with syntactic sugar for assignment to members.
+  Spacecraft(this.name, this.launchDate) {
+    // Initialization code goes here.
+  }
+
+  // Named constructor that forwards to the default one.
+  Spacecraft.unlaunched(String name) : this(name, null);
+
+  // Method.
+  void describe() {
+    print('Spacecraft: $name');
+    // Type promotion doesn't work on getters.
+    var launchDate = this.launchDate;
+    if (launchDate != null) {
+      int years = DateTime.now().difference(launchDate).inDays ~/ 365;
+      print('Launched: $launchYear ($years years ago)');
+    } else {
+      print('Unlaunched');
+    }
+  }
+}
+
+enum PlanetType { terrestrial, gas, ice }
+
+enum Planet {
+  mercury(planetType: PlanetType.terrestrial, moons: 0, hasRings: false),
+  venus(planetType: PlanetType.terrestrial, moons: 0, hasRings: false),
+  uranus(planetType: PlanetType.ice, moons: 27, hasRings: true),
+  neptune(planetType: PlanetType.ice, moons: 14, hasRings: true),
+  earth(planetType: PlanetType.terrestrial, moons: 1, hasRings: false);
+
+  const Planet(
+      {required this.planetType, required this.moons, required this.hasRings});
+
+  final PlanetType planetType;
+  final int moons;
+  final bool hasRings;
+
+  bool get isGiant =>
+      planetType == PlanetType.gas || planetType == PlanetType.ice;
+}
+
+// Inheritance
+// #
+class Orbiter extends Spacecraft {
+  double altitude;
+
+  Orbiter(super.name, DateTime super.launchDate, this.altitude);
+}
+
+// Mixins
+mixin Piloted {
+  int astronauts = 1;
+
+  void describeCrew() {
+    print('Number of astronauts: $astronauts');
+  }
+}
+
+class PilotedCraft extends Spacecraft with Piloted {
+  PilotedCraft(super.name, DateTime super.launchDate);
+}
+
+class MockSpaceship implements Spacecraft {
+  MockSpaceship(this.name, this.launchDate);
+
+  @override
+  DateTime? launchDate;
+
+  @override
+  String name;
+
+  @override
+  void describe() {
+    // TODO: implement describe
+  }
+
+  @override
+  // TODO: implement launchYear
+  int? get launchYear => throw UnimplementedError();
+}
+
+abstract class Describable {
+  void describe();
+
+  void describeWithEmphasis() {
+    print('=========');
+    describe();
+    print('=========');
+  }
+}
+
+const oneSecond = Duration(seconds: 3);
+
+Future<void> printWithDelay(String message) async {
+  return Future.delayed(oneSecond).then((_) => print(message));
+}
+
+Future<void> createDescriptions(Iterable<String> objects) async {
+  for (final object in objects) {
+    try {
+      var file = File('$object.txt');
+      if (await file.exists()) {
+        var modified = await file.lastModified();
+        print('File for $object already exists. It was modified on $modified.');
+        continue;
+      }
+      await file.create();
+      await file.writeAsString('Start describing $object in this file.');
+    } on IOException catch (e) {
+      print('Cannot create description for $object: $e');
+    }
+  }
+}
+
+Stream<String> report(Spacecraft craft, Iterable<String> objects) async* {
+  for (final object in objects) {
+    await Future.delayed(oneSecond);
+    yield '${craft.name} flies by $object';
+  }
+}
+
+Future<void> describeFlyObjects(List<String> flybyObjects) async {
+  try {
+    for (final object in flybyObjects) {
+      var description = await File('$object.txt').readAsString();
+      print(description);
+    }
+  } on IOException catch (e) {
+    print('Cloud not describe object: $e');
+  } finally {
+    flybyObjects.clear();
+  }
+}
